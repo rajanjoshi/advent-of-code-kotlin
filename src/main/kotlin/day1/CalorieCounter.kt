27b47de1a91391b9c8e781b75abd03ac695bb45c
rajanjoshi <joshirajan999@gmail.com>
2023-03-18 21:28:47 +0530

    Create CalorieCounter.kt

---
+++ src/main/kotlin/day1/CalorieCounter.kt
@@ -0,0 +1,23 @@
+import java.io.File
+
+private const val RESOURCE_ROOT = "src/main/resources"
+
+fun main() {
+    fun part1(input: String) = input.split("\r\n\r\n").map { line ->
+        line.split("\r\n").filter { it.isNotBlank() }.map { cal -> cal.toInt() }
+    }.maxOfOrNull { it.sum() }
+
+    fun part2(input: String) = input.split("\r\n\r\n").map { line ->
+        line.split("\r\n").filter { it.isNotBlank() }.map { cal -> cal.toInt() }
+    }.map { it.sum() }.sortedDescending().take(3).sum()
+
+    val testInput = readInput("day1/test")
+    check(part1(testInput) == 24000)
+
+    val input = readInput("day1/input")
+    println(part1(input))
+    println(part2(input))
+}
+
+
+fun readInput(name: String) = File(RESOURCE_ROOT, "$name.txt").readText()
